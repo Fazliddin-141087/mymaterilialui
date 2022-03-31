a982d37b0d5adc130ff7631758dc2594d82a816c
Fazliddin141087 <fazliddin-xamdamov@mail.ru>
2022-03-31 18:40:26 +0500

    this i am picker

--- mymaterialdesignui/src/main/java/uz/mobiler/mymaterialdesignui/CalendarDialog.kt
+++ mymaterialdesignui/src/main/java/uz/mobiler/mymaterialdesignui/CalendarDialog.kt
@@ -1,11 +1,13 @@
 package uz.mobiler.mymaterialdesignui
 
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.datepicker.MaterialDatePicker
 
 object CalendarDialog {
-    fun show(title: String) {
-        MaterialDatePicker.Builder.datePicker()
+    fun show(title: String,fragmentManager: FragmentManager) {
+        var datapicker= MaterialDatePicker.Builder.datePicker()
             .setTitleText(title)
             .build()
+        datapicker.show(fragmentManager,"tag")
     }
 }
